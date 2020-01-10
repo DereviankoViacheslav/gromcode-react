d2960366628cd036cda6e6b66a15a54aa411d32e
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-10 14:56:02 +0200

    add lesson01 task2

--- lesson01/task2/index.js
+++ lesson01/task2/index.js
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+const rootElem = document.querySelector('#root');
+
+const element = React.createElement(
+  'div',
+  { className: 'greeting' },
+  React.createElement(
+    'h1',
+    { className: 'greeting__title' },
+    'Hello, world!'
+  ),
+  React.createElement(
+    'p',
+    { className: 'greeting__text' },
+    'I\'m learning React'
+  ),
+);
+
+ReactDOM.render(element, rootElem);
