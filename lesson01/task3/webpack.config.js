3aa18b0caa90def78709c2ac48acd9affe358edc
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-11 15:31:27 +0200

    add lesson02 task3

--- lesson01/task3/webpack.config.js
+++ lesson01/task3/webpack.config.js
@@ -1,25 +1,25 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /.js$/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /.s?css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template:'./src/index.html'
-        }),
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
 };
