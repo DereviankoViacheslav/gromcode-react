405d93b24d83a0a77b2fb65056751be336997f13
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-25 20:45:13 +0200

    add lesson10 hw2

---
+++ lesson10/hw2/src/ProductRow.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProductRow({ product }) {
+  const { name, price, stocked } = product;
+
+  return (
+    <tr>
+      <td>{stocked ? name : <span style={{ color: 'red' }}>{name}</span>}</td>
+      <td>{price}</td>
+    </tr>
+  );
+}
+
+export default ProductRow;
