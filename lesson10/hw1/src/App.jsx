9808fb056a8d9691cbf75f1b6bd3e93659e04e38
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-25 19:51:44 +0200

    add lesson10 hw1

---
+++ lesson10/hw1/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Calculator from './Calculator';
+import './app.scss';
+
+function App() {
+  return (
+    <Calculator />
+  );
+}
+
+export default App;
---
+++ lesson10/hw1/src/TemperatureInput.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+class TemperatureInput extends Component {
+  handleChange = event => {
+    this.props.onTemperatureChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[this.props.scale]}:</legend>
+        <input value={this.props.temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput;
