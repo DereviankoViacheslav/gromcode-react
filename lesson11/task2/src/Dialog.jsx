56d621a56acb26619d409a37fd2e529798122d08
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-27 20:41:50 +0200

    add lesson10 task2

---
+++ lesson11/task2/src/Dialog.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Dialog({ title, onClose, children }) {
+  return (
+    <div className="dialog">
+      <div className="dialog__heading">
+        <h4 className="dialog__title">{title}</h4>
+        <button onClick={onClose} className="dialog__close-btn">+</button>
+      </div>
+      <div className="dialog__content">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+Dialog.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.element.isRequired,
+  onClose: PropTypes.func.isRequired,
+}
+
+Dialog.defaultProps = {
+  title: '',
+}
+
+export default Dialog;
