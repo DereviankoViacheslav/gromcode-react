6cb140a11f4132751476d0ac8b209219d662e82a
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-01-29 18:53:07 +0200

    add lesson12 todo-list

---
+++ lesson12/todo-list/build/precache-manifest.e8626c57a8310a017acdaa6e16fe7ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6957e150fa921c0db4db5f228c9a6896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f83459e1b0e56727abd8",
+    "url": "/static/css/main.bc7b3a32.chunk.css"
+  },
+  {
+    "revision": "0402e2509a936ebad6d9",
+    "url": "/static/js/2.401686de.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.401686de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f83459e1b0e56727abd8",
+    "url": "/static/js/main.02859066.chunk.js"
+  },
+  {
+    "revision": "b6aab384b0c1e14ae267",
+    "url": "/static/js/runtime-main.d2c0771e.js"
+  }
+]);
